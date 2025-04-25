d7da6819c21107ffbfb6d055d3ac327927fa5677
aliNassef <anassef798@gmail.com>
2025-04-25 09:22:55 +0300

    fix: update initial route to WelcomeView for onboarding flow

--- lib/fit_track_app.dart
+++ lib/fit_track_app.dart
@@ -1,9 +1,9 @@
 import 'package:device_preview_plus/device_preview_plus.dart';
-import 'package:fit_track_app/features/workout/presentation/view/workout_excersises_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'core/utils/app_router.dart';
 import 'core/utils/app_themes.dart';
+import 'features/onboarding/presentation/views/welcome_view.dart';
 
 class FitTrackApp extends StatelessWidget {
   const FitTrackApp({super.key});
@@ -22,7 +22,7 @@ class FitTrackApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           theme: getLightTheme(),
           onGenerateRoute: (settings) => onGenerateRoute(settings),
-          initialRoute: WorkoutExerciseView.routeName,
+          initialRoute: WelcomeView.routeName,
         );
       },
     );
