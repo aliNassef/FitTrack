1100be58973bfdedff1d849824ddb67127068f4b
aliNassef <anassef798@gmail.com>
2025-04-30 15:42:36 +0300

    refactor(meal_planner): wrap column in SingleChildScrollView for better scrollability
    The Column widget in CategoryMealView was wrapped with SingleChildScrollView to ensure the content remains scrollable when it exceeds the screen height. This improves the user experience by preventing overflow issues on smaller screens.

--- lib/features/meal_planner/presentation/view/category_meal_view.dart
+++ lib/features/meal_planner/presentation/view/category_meal_view.dart
@@ -2,7 +2,6 @@ import 'package:fit_track_app/core/extensions/padding_extension.dart';
 import 'package:fit_track_app/core/helpers/app_spacer.dart';
 import 'package:fit_track_app/core/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
-
 import '../../../../core/utils/app_styles.dart';
 import '../widgets/category_list_items.dart';
 import '../widgets/custom_search_bar.dart';
@@ -16,35 +15,37 @@ class CategoryMealView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const CustomAppBar(title: 'Breakfast'),
-            const VerticalSpace(34),
-            const CustomSearchBar(),
-            const VerticalSpace(30),
-            Text(
-              'Category',
-              style: AppStyles.semiBold16,
-            ),
-            const VerticalSpace(15),
-            const CategoryListItems(),
-            const VerticalSpace(30),
-            Text(
-              'Recommendation for Diet',
-              style: AppStyles.semiBold16,
-            ),
-            const VerticalSpace(15),
-            const RecommendedMealListItems(),
-            const VerticalSpace(30),
-            Text(
-              'Popular',
-              style: AppStyles.semiBold16,
-            ),
-            const VerticalSpace(15),
-            const PopularMealItem(),
-          ],
-        ).withHorizontalPadding(30),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const CustomAppBar(title: 'Breakfast'),
+              const VerticalSpace(34),
+              const CustomSearchBar(),
+              const VerticalSpace(30),
+              Text(
+                'Category',
+                style: AppStyles.semiBold16,
+              ),
+              const VerticalSpace(15),
+              const CategoryListItems(),
+              const VerticalSpace(30),
+              Text(
+                'Recommendation for Diet',
+                style: AppStyles.semiBold16,
+              ),
+              const VerticalSpace(15),
+              const RecommendedMealListItems(),
+              const VerticalSpace(30),
+              Text(
+                'Popular',
+                style: AppStyles.semiBold16,
+              ),
+              const VerticalSpace(15),
+              const PopularMealItem(),
+            ],
+          ).withHorizontalPadding(30),
+        ),
       ),
     );
   }
