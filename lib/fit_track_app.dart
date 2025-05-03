df308f408be6d046742da40b7f45946dd1ab886b
aliNassef <anassef798@gmail.com>
2025-05-04 00:41:27 +0300

    refactor: change initial route from StatisticsView to WelcomeView
    This change aligns the app's initial route with the onboarding flow, ensuring users are directed to the WelcomeView first. The StatisticsView import was removed as it is no longer needed in this file.

--- lib/fit_track_app.dart
+++ lib/fit_track_app.dart
@@ -4,7 +4,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'core/utils/app_router.dart';
 import 'core/utils/app_themes.dart';
 import 'features/onboarding/presentation/views/welcome_view.dart';
-import 'features/profile/presentation/views/statistics_view.dart';
 
 class FitTrackApp extends StatelessWidget {
   const FitTrackApp({super.key});
@@ -23,7 +22,7 @@ class FitTrackApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           theme: getLightTheme(),
           onGenerateRoute: (settings) => onGenerateRoute(settings),
-          initialRoute: StatisticsView.routeName,
+          initialRoute: WelcomeView.routeName,
         );
       },
     );
