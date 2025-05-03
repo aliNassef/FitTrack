93046b67c335993001b3771071f736dbae506297
aliNassef <anassef798@gmail.com>
2025-05-03 18:33:05 +0300

    refactor(home_grid_item): wrap container with GestureDetector for tap handling
    The Container widget in HomeGridItem is now wrapped with a GestureDetector to handle tap events, replacing the previous button-based navigation. This improves the user experience by making the entire grid item clickable.

--- lib/features/home/presentation/widgets/home_grid_item.dart
+++ lib/features/home/presentation/widgets/home_grid_item.dart
@@ -1,3 +1,4 @@
+
 import 'package:fit_track_app/core/utils/app_images.dart';
 import 'package:fit_track_app/features/home/data/models/home_model.dart';
 import 'package:fit_track_app/features/meal_planner/presentation/view/meal_planner_view.dart';
@@ -19,73 +20,76 @@ class HomeGridItem extends StatelessWidget {
   final int index;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: const Color(0xffFFFFFF),
-        borderRadius: BorderRadius.circular(20.r),
-        boxShadow: AppShadows.shadow1,
-      ),
-      width: 160.w,
-      padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
-      child: Stack(
-        children: [
-          Text(
-            instance.title,
-            style: AppStyles.medium14.copyWith(
-              color: const Color(0xff000000),
+    return GestureDetector(
+      onTap: () {
+        _goToSelectedPage(context);
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: const Color(0xffFFFFFF),
+          borderRadius: BorderRadius.circular(20.r),
+          boxShadow: AppShadows.shadow1,
+        ),
+        width: 160.w,
+        padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
+        child: Stack(
+          children: [
+            Text(
+              instance.title,
+              style: AppStyles.medium14.copyWith(
+                color: const Color(0xff000000),
+              ),
+              textAlign: TextAlign.center,
             ),
-            textAlign: TextAlign.center,
-          ),
-          const VerticalSpace(7),
-          Positioned(
-            top: 50.h,
-            left: 0,
-            right: 0,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: 20.h,
-                  width: 50.w,
-                  child: DefaultAppButton(
-                    text: 'Go To',
-                    textStyle: AppStyles.semiBold10.copyWith(
-                      color: Colors.white,
-                    ),
-                    onPressed: () {
-                      _goToSelectedPage(context);
-                    },
-                  ),
-                ),
-                ShaderMask(
-                  shaderCallback: (bounds) => const LinearGradient(
-                    colors: [
-                      Color(0xff263238),
-                      Color(0xffC150F6),
-                    ],
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                  ).createShader(
-                    Rect.fromLTWH(0, 0, bounds.width, bounds.height),
-                  ),
-                  child: Text(
-                    ' ${instance.subTitle}',
-                    style: AppStyles.semiBold10.copyWith(
-                      color: Colors.white,
+            const VerticalSpace(7),
+            Positioned(
+              top: 50.h,
+              left: 0,
+              right: 0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 20.h,
+                    width: 50.w,
+                    child: DefaultAppButton(
+                      text: 'Go To',
+                      textStyle: AppStyles.semiBold10.copyWith(
+                        color: Colors.white,
+                      ),
+                      onPressed: () {},
                     ),
                   ),
-                ),
-              ],
+                  ShaderMask(
+                    shaderCallback: (bounds) => const LinearGradient(
+                      colors: [
+                        Color(0xff263238),
+                        Color(0xffC150F6),
+                      ],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                    ).createShader(
+                      Rect.fromLTWH(0, 0, bounds.width, bounds.height),
+                    ),
+                    child: Text(
+                      ' ${instance.subTitle}',
+                      style: AppStyles.semiBold10.copyWith(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          Positioned(
-            bottom: 0.h,
-            child: Image.asset(
-              alignment: Alignment.topCenter,
-              AppImages.homeBackgroundItem,
+            Positioned(
+              bottom: 0.h,
+              child: Image.asset(
+                alignment: Alignment.topCenter,
+                AppImages.homeBackgroundItem,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
